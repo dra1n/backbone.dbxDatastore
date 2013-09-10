edcd5d8122f6671862bcdbe7c5e5bf0409d0bd07
Andrey Bakuta <anb@anahoret.com>
2013-09-10 14:54:23 +0300

    Implemented some stuff.

--- dist/backbone.dbxDatastore.js
+++ dist/backbone.dbxDatastore.js
@@ -15,23 +15,113 @@
     }
   })(this, function(_, Backbone) {
     Backbone.DbxDatastore = (function() {
-      function DbxDatastore(datastore, name) {
-        'rofl';
+      function DbxDatastore(tableName) {
+        this.tableName = tableName;
       }
 
-      DbxDatastore.prototype.save = function() {};
+      DbxDatastore.prototype.create = function(model) {
+        var record;
+        record = this._getTable().insert(model.toJSON());
+        model.id = record.getId();
+        model.set(model.idAttribute, model.id);
+        return this.find(model);
+      };
 
-      DbxDatastore.prototype.create = function() {};
+      DbxDatastore.prototype.update = function(model) {
+        this._getRecord(model).update(model.toJSON());
+        return this.find(model);
+      };
 
-      DbxDatastore.prototype.update = function() {};
+      DbxDatastore.prototype.find = function(model) {
+        return this.jsonData(this._getRecord(model));
+      };
 
-      DbxDatastore.prototype.find = function() {};
+      DbxDatastore.prototype.findAll = function() {
+        var _this = this;
+        return _(this._getTable().query()).map(function(r) {
+          return _this.jsonData(r);
+        });
+      };
 
-      DbxDatastore.prototype.findAll = function() {};
+      DbxDatastore.prototype.destroy = function(model) {
+        if (model.isNew()) {
+          return false;
+        }
+        this._getRecord(model).deleteRecord();
+        return model;
+      };
 
-      DbxDatastore.prototype.destroy = function() {};
+      DbxDatastore.prototype.jsonData = function(record) {
+        var data;
+        if (record != null) {
+          data = record.getFields();
+          data.id = record.getId();
+        }
+        return data;
+      };
 
-      DbxDatastore.sync = function(method, model, options) {};
+      DbxDatastore.prototype._getRecord = function(model) {
+        return this._getTable().get(model.id);
+      };
+
+      DbxDatastore.prototype._getTable = function() {
+        return this._table || (this._table = Backbone.DbxDatastore.datastore.getTable(this.tableName));
+      };
+
+      DbxDatastore.sync = function(method, model, options) {
+        var datastore, error, errorMessage, resp, store, syncDfd;
+        datastore = Backbone.DbxDatastore.datastore;
+        store = model.dbxDatastore || model.collection.DbxDatastore;
+        syncDfd = Backbone.$.Deferred && Backbone.$.Deferred();
+        try {
+          switch (method) {
+            case 'read':
+              resp = model.id != null ? store.find(model) : store.findAll();
+              break;
+            case 'create':
+              resp = store.create(model);
+              break;
+            case 'update':
+              resp = store.update(model);
+              break;
+            case 'delete':
+              resp = store.destroy(model);
+          }
+        } catch (_error) {
+          error = _error;
+          errorMessage = error.message;
+        }
+        if (resp != null) {
+          if ((options != null ? options.success : void 0)) {
+            if (Backbone.VERSION === '0.9.10') {
+              options.success(model, resp, options);
+            } else {
+              options.success(resp);
+            }
+          }
+          if (syncDfd != null) {
+            syncDfd.resolve(resp);
+          }
+        } else {
+          errorMessage = errorMessage != null ? errorMessage : 'Record Not Found';
+          if (options != null ? options.error : void 0) {
+            if (Backbone.VERSION === '0.9.10') {
+              options.error(model, errorMessage, options);
+            } else {
+              options.error(errorMessage);
+            }
+          }
+          if (syncDfd != null) {
+            syncDfd.reject(errorMessage);
+          }
+        }
+        if ((options != null ? options.complete : void 0)) {
+          options.complete(resp);
+        }
+        return syncDfd != null ? syncDfd.promise() : void 0;
+      };
+
+      DbxDatastore.datastore = null;
 
       return DbxDatastore;
 
@@ -40,7 +130,7 @@
     Backbone.getSyncMethod = function(model) {
       var _ref;
       if (model.dbxDatastore || ((_ref = model.collection) != null ? _ref.dbxDatastore : void 0)) {
-        return Backbone.dbxDatastore.sync;
+        return Backbone.DbxDatastore.sync;
       } else {
         return Backbone.ajaxSync;
       }
--- src/backbone.dbxDatastore.coffee
+++ src/backbone.dbxDatastore.coffee
@@ -4,28 +4,99 @@ do (
   root = this,
   factory = (_, Backbone) ->
     class Backbone.DbxDatastore
-      constructor: (datastore, name) ->
-        'rofl'
+      constructor: (@tableName) ->
 
-      save: () ->
 
-      create: () ->
+      create: (model) ->
+        record = @_getTable().insert model.toJSON()
+        model.id = record.getId()
+        model.set(model.idAttribute, model.id)
+        @find(model)
 
-      update: () ->
 
-      find: () ->
+      update: (model) ->
+        @_getRecord(model).update(model.toJSON())
+        @find(model)
 
-      findAll: () ->
 
-      destroy: () ->
+      find: (model) ->
+        @jsonData(@_getRecord(model))
+
+
+      findAll: ->
+        _(@_getTable().query()).map (r) => @jsonData(r)
+
+
+      destroy: (model) ->
+        return false if model.isNew()
+        @_getRecord(model).deleteRecord()
+        model
+
+
+      jsonData: (record) ->
+        if record?
+          data = record.getFields()
+          data.id = record.getId()          # *idAttribute* is hardcoded to *id*
+        data
+
+
+      _getRecord: (model) ->
+        @_getTable().get(model.id)
+
+
+      _getTable: ->
+        @_table or= Backbone.DbxDatastore.datastore.getTable(@tableName)
+
 
       @sync: (method, model, options) ->
+        datastore = Backbone.DbxDatastore.datastore
+        store = model.dbxDatastore || model.collection.DbxDatastore
+        syncDfd = Backbone.$.Deferred && Backbone.$.Deferred()
+
+        try
+          switch method
+            when 'read'
+              resp = if model.id? then store.find(model) else store.findAll()
+            when 'create'
+              resp = store.create(model)
+            when 'update'
+              resp = store.update(model)
+            when 'delete'
+              resp = store.destroy(model)
+
+        catch error
+          errorMessage = error.message
+
+        if resp?
+          if (options?.success)
+            if Backbone.VERSION is '0.9.10'
+              options.success(model, resp, options)
+            else
+              options.success(resp)
+
+          if syncDfd? then syncDfd.resolve(resp)
+        else
+          errorMessage = if errorMessage? then errorMessage else 'Record Not Found'
+
+          if options?.error
+            if Backbone.VERSION is '0.9.10'
+              options.error(model, errorMessage, options)
+            else
+              options.error(errorMessage)
+          if syncDfd? then syncDfd.reject(errorMessage)
+
+        if (options?.complete) then options.complete(resp)
+
+        syncDfd?.promise()
+
+
+      @datastore: null
 
     Backbone.ajaxSync = Backbone.sync
 
     Backbone.getSyncMethod = (model) ->
       if (model.dbxDatastore or model.collection?.dbxDatastore)
-        Backbone.dbxDatastore.sync
+        Backbone.DbxDatastore.sync
       else
         Backbone.ajaxSync
 
